845aaab8c58cdac8bcbd447e9154467c6566c3c6
jjhna <hungryhungryjoejna@gmail.com>
2017-02-22 22:19:31 +0000

    Task 2: Templates

---
+++ app/imports/ui/body.js
@@ -0,0 +1,16 @@
+/**
+ * Created by jojona on 2/22/17.
+ * wod
+ */
+
+import { Template } from 'meteor/templating';
+
+import './body.html';
+
+Template.body.helpers({
+  tasks: [
+    { text: 'This is task 1' },
+    { text: 'This is task 2' },
+    { text: 'This is task 3' },
+  ],
+});
